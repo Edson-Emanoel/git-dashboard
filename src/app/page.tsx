38fd2bc4232cafc6d9a6230882651c0f2f9259b1
Edson-Emanoel <edson.emanoelofc@gmail.com>
2024-11-01 09:24:22 -0300

    Terminei o Crud

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 "use client"
-
-import CadastroGestor from '@/components/Form';
-// pages/post.js
-import { createClient } from '@supabase/supabase-js';
-import { log } from 'console';
+import '../components/Form/gestor.css'
 import { useEffect, useState } from 'react';
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON;
@@ -17,7 +14,21 @@ type TypeGestor = {
   created_at: string
 }
 
+interface IGestor {
+  id: any
+  nome: string
+  sexo: string
+}
+
 export default function Home() {
+  const [id, setId] = useState<string>('');
+  const [nome, setNome] = useState<string>('');
+  const [sexo, setSexo] = useState<string>('');
+  const [mensagem, setMensagem] = useState<string>('');
+
+  const [editar, setEditar] = useState('sumir');
+  const [cadastrar, setCadastrar] = useState('');
+  
   const [gestor, setGestor] = useState<TypeGestor[]>([]);
 
   const fetchGestor = async () => {
@@ -30,6 +41,56 @@ export default function Home() {
       console.error('Error fetching posts:', error);
     } else {
       setGestor(data);
+    }
+  };
+
+  const cadastrarGestor = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const { data, error } = await supabase
+        .from('gestores')
+        .insert([{ nome, sexo }]);
+
+      if (error) {
+        setMensagem(`Erro ao cadastrar gestor: ${error.message}`);
+      } else {
+        setMensagem(`Gestor ${nome} cadastrado com sucesso!`);
+        setNome('');
+        setSexo('');
+      }
+    } catch (error) {
+      setMensagem(`Ocorreu um erro: ${error}`);
+    }
+  };
+
+  const carregarCampos = (id: any, nome: string, sexo: string) => {
+    setId(id)
+    setNome(nome)
+    setSexo(sexo)
+
+    setCadastrar('sumir')
+    setEditar('')
+  }
+
+  const handleEditar = async () => {
+    const { error } = await supabase
+      .from('gestores')
+      .update({ nome, sexo })
+      .match({ id: id });
+
+    if (error) {
+      // setError('Erro ao atualizar gestor');
+      console.error(error);
+    } else {
+      alert('Gestor atualizado com sucesso!');
+
+      setId('')
+      setNome('')
+      setSexo('')
+
+      setCadastrar('')
+      setEditar('sumir')
     }
   };
 
@@ -49,33 +110,65 @@ export default function Home() {
     } catch (error) {
       console.log(`Ocorreu um erro :(`);
     }
-};
+  };
 
   useEffect(() => {
     fetchGestor(); // Chama a função ao carregar o componente
-  }, []);
+  }, [gestor, setGestor]);
 
   return (
     <main className='p-5 px-28'>
       <h1 className="font-medium text-3xl">Shadcn-UI</h1>
 
       <div className="listar mr-20">
-      <CadastroGestor />
+        <h1 className='text-center text-3xl font-bold mb-5'>Cadastro de Gestores</h1>
 
-      <h2 className='text-center text-3xl font-bold mt-5 mb-5'>Lista de Gestores</h2>
-      <div className='flex gap-2 flex-col '>
-        {gestor.map( item => (
-          <div key={item.id} className='bg-green-500 p-2 rounded-md text-white'>
-            <h3>{item.nome}</h3>
-            <p>{item.sexo}</p>
-            <small>{new Date(item.created_at).toLocaleString()}</small>
-            <button onClick={() => deletarGestor(item.id)} className='ml-28 bg-red-600 w-20 rounded-md'>Deletar</button>
+        <form onSubmit={cadastrarGestor} className='flex flex-col items-center justify-center gap-3'>
+          <div className='flex flex-col'>
+            <label className='text-xl font-medium'>Nome:</label>
+            <input
+              type="text"
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
+              className='px-2 border-none outline-none bg-zinc-800 text-white w-80 h-8 rounded-md text-base font-normal'
+              required
+            />
           </div>
-        ))}
-      </div>
+          <div className='flex flex-col'>
+              <label className='text-xl font-medium'>Sexo:</label>
+              <select
+                value={sexo}
+                onChange={(e) => setSexo(e.target.value)}
+                className='px-2 border-none outline-none bg-zinc-800 text-white w-80 h-8 rounded-md text-base font-normal'
+                required
+              >
+                <option value="">Selecione</option>
+                <option value="Masculino">Masculino</option>
+                <option value="Feminino">Feminino</option>
+              </select>
+          </div>
+          <div className="flex items-center justify-center gap-2">
+            <button type="submit" className={`bg-blue-700 text-white text-lg font-medium rounded-md w-40 h-8 ${cadastrar}`}>Cadastrar</button>
+            <button type="button" className={`bg-yellow-500 text-white text-lg font-medium rounded-md w-40 h-8 ${editar}`} onClick={() => handleEditar()}>Salvar</button>
+          </div>
+        </form>
+        {mensagem && <p>{mensagem}</p>}
 
+          <h2 className='text-center text-3xl font-bold mt-5 mb-5'>Lista de Gestores</h2>
+          <div className='flex gap-2 flex-col'>
+          {
+            gestor.map((item) => (
+              <div key={item.id} className='bg-green-500 p-2 rounded-md text-white'>
+                <h3>{item.nome}</h3>
+                <p>{item.sexo}</p>
+                <small>{new Date(item.created_at).toLocaleString()}</small>
+                <button onClick={() => deletarGestor(item.id)} className='ml-28 bg-red-600 w-20 rounded-md'>Deletar</button>
+                <button onClick={() => carregarCampos(item.id, item.nome, item.sexo)} className='ml-5 bg-yellow-500 w-20 rounded-md'>Editar</button>
+              </div> 
+            ))
+          }
+          </div>
       </div>
-
     </main>
   );
 }
--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,5 +1,5 @@
 import './gestor.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { createClient } from '@supabase/supabase-js';
 
 // Configure suas credenciais do Supabase
@@ -7,61 +7,39 @@ const supabaseUrl = 'https://tqvkkwmqtyyzajeqnkwv.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzQ5OTY4NCwiZXhwIjoxOTU5MDc1Njg0fQ.J1C20mhPGJqoCbhY_oEACEDd7Rdv2gvTVR98j7OBF6Y';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-const CadastroGestor: React.FC = () => {
-    const [nome, setNome] = useState<string>('');
-    const [sexo, setSexo] = useState<string>('');
-    const [mensagem, setMensagem] = useState<string>('');
+interface EditarGestorProps {
+    gestorId: string; // ID do gestor que será editado
+}
 
-    const cadastrarGestor = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+const CadastroGestor: React.FC<EditarGestorProps> = ({gestorId}) => {
+    // const [id, setId] = useState<string>('')
+    // setId(gestorId);
 
-        try {
-            const { data, error } = await supabase
-                .from('gestores')
-                .insert([{ nome, sexo }]);
+    
 
-            if (error) {
-                setMensagem(`Erro ao cadastrar gestor: ${error.message}`);
-            } else {
-                setMensagem(`Gestor ${nome} cadastrado com sucesso!`);
-                setNome('');
-                setSexo('');
-            }
-        } catch (error) {
-            setMensagem(`Ocorreu um erro: ${error}`);
-        }
-    };
+    // useEffect(() => {
+    //     const editarGestor = async ({ gestorId }: EditarGestorProps) => {
+    //         const { data, error } = await supabase
+    //             .from('gestores')
+    //             .select('nome, sexo')
+    //             .eq('id', gestorId)
+    //             .single();
+
+    //         if (error) {
+    //             console.log('Erro ao carregar gestor');
+    //             console.error(error);
+    //         } else {
+    //             setNome(data.nome);
+    //             setSexo(data.sexo);
+    //         }
+    //     };
+
+    //     editarGestor(id);
+    // }, [gestorId]);    
 
     return (
         <div className='container'>
-            <h1 className='text-center text-3xl font-bold mb-5'>Cadastro de Gestores</h1>
-            <form onSubmit={cadastrarGestor} className='flex flex-col items-center justify-center gap-3'>
-                <div className='flex flex-col'>
-                    <label className='text-xl font-medium'>Nome:</label>
-                    <input
-                        type="text"
-                        value={nome}
-                        onChange={(e) => setNome(e.target.value)}
-                        className='px-2 border-none outline-none bg-zinc-800 text-white w-80 h-8 rounded-md text-base font-normal'
-                        required
-                    />
-                </div>
-                <div className='flex flex-col'>
-                    <label className='text-xl font-medium'>Sexo:</label>
-                    <select
-                        value={sexo}
-                        onChange={(e) => setSexo(e.target.value)}
-                        className='px-2 border-none outline-none bg-zinc-800 text-white w-80 h-8 rounded-md text-base font-normal'
-                        required
-                    >
-                        <option value="">Selecione</option>
-                        <option value="Masculino">Masculino</option>
-                        <option value="Feminino">Feminino</option>
-                    </select>
-                </div>
-                <button type="submit" className='bg-blue-700 text-white text-lg font-medium rounded-md w-52 h-8'>Cadastrar</button>
-            </form>
-            {mensagem && <p>{mensagem}</p>}
+            
         </div>
     );
 };
